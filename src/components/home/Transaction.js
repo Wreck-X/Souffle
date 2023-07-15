5a7979dbd419b0a0e231d35c60c9194c2290a5e2
LiquidFlare <ivinjoelabraham@gmail.com>
2023-07-15 12:03:18 +0530

    adds basic card from nextui

--- src/components/home/Transaction.js
+++ src/components/home/Transaction.js
@@ -5,18 +5,22 @@ function Transaction(props) {
     if (props.income) {
         return (
             <>
+                <Card css={{ mw: "400px" }} isHoverable="true" isPressable="true">
                 <div>{props.amount}</div>
                 <div>Income</div>
                 <div>{props.id}</div>
+                </Card >
             </>
         )
     } else {
         return (
             <>
+                <Card css={{ mw: "400px", mh: "200px" }}>
                 <div>{props.amount}</div>
                 <div>Outcome</div>
                 <div>{props.id}</div>
                 <div>{props.payee}</div>
+                </Card>
             </>
         )
     }
