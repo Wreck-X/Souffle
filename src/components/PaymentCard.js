d75d17f7289d5c9efcc5e83540c6df616470f4cd
spellsharp <svcodin22@gmail.com>
2023-07-16 02:11:11 +0530

    Added a bank ID to the payment card

--- src/components/PaymentCard.js
+++ src/components/PaymentCard.js
@@ -8,7 +8,7 @@ function PayPal() {
       <img
         src="https://rangers.co.th/wp-content/uploads/2018/09/paypal-logotype-png-510.png"
         alt="Secure payment in PayPal"
-        className="h-[6rem] w-[13rem] ml-[88vw]"
+        className="h-[6rem] w-[13rem]"
       />
     </>
   );
@@ -17,7 +17,7 @@ function PayPal() {
 function Input(props) {
   return (
     <>
-      <div className="flex flex-col mt-20">
+      <div className="flex flex-col mt-10">
         <label className="text-xl">{props.label}</label>
         <input
           placeholder={props.placeholder}
@@ -46,13 +46,19 @@ function PaymentCard(props) {
               />
               <Input label="Amount (in USD)" placeholder="Enter amount" />
             </div>
-            <Button className="mx-auto w-[10rem] h-[3rem] rounded-full mt-[5rem] text-black outline outline-1">
-              Transfer
-            </Button>
+            <div className="flex flex-col mt-14">
+              <div className="grid place-content-center h-full">
+                <div class="btn btn-one flex justify-center items-center text-[23px] w-[8rem] h-[4rem]">
+                  <span>TRANSFER</span>
+                </div>
+              </div>
+            </div>
           </div>
         </Card>
       </div>
-      <PayPal />
+      <div className="flex justify-end">
+        <PayPal />
+      </div>
     </>
   );
 }
