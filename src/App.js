5b26d2099edc62008d6c28d2bb0151edcdd0ff44
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-07-15 22:08:52 +0530

    button fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Landing from './pages/Landing';
-
+import './index.css';
 const App = () => {
     return (
         <>
