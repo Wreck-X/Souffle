dd4d09b11fb6170df000c958b78ee4259b5b1b11
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-07-15 20:11:00 +0530

    refactoring

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ import Search from './components/home/Search';
 import CharityPage from './components/home/CharityPage';
 import Graph from './components/home/Graph';
 import Landing from './components/home/Landing';
+import Home from './components/home/Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Graph></Graph>
+    <Home></Home>
   </React.StrictMode>
 );
 
