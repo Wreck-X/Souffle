047ee869d00c15940000af1563d7a568423c5b0f
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-07-15 22:57:03 +0530

    charitytransaction structure, Top bar changes,charityc card changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Landing from './pages/Landing';
 import './index.css';
+import CharityTransactions from './pages/CharityTransactions';
 const App = () => {
     return (
         <>
             <Routes>
                 <Route path="/" element={<Landing />} />
                 <Route path="/home" element={<Home />} />
+                <Route path="/transactions" element={<CharityTransactions />} />
             </Routes>
         </>
     );
