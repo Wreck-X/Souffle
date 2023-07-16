e97a0db2de35389e53d0e41fa90cb914d547e2ea
spellsharp <svcodin22@gmail.com>
2023-07-16 15:59:09 +0530

    SuccessPage added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Payment from './pages/Payment';
 import './index.css';
 import CharityTransactions from './pages/CharityTransactions';
 import { useEffect } from 'react';
+import SuccessfulTransaction from './pages/SuccessfulTransaction';
 
 const App = () => {
     return (
@@ -14,7 +15,7 @@ const App = () => {
                 <Route path="/home" element={<Home />} />
                 <Route path="/payment" element={<Payment />} />
                 <Route path="/transactions" element={<CharityTransactions />} />
-
+                <Route path="/successful" element={<SuccessfulTransaction />} />
             </Routes>
         </>
     );
---
+++ src/pages/SuccessfulTransaction.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SuccessfulTransaction = () => {
+  return (
+    <div className='flex justify-center items-center'>
+        <img src ="https://cdn.dribbble.com/users/911154/screenshots/3332845/media/20e5d28e19e4f52c634fd3786a5a1a28.gif" alt="Successful"/>
+    </div>
+  )
+}
+
+export default SuccessfulTransaction
